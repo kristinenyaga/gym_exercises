60381a5b312ac78891d322ff17c28c5b2b4b6f18
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-26 22:41:37 +0300

    bodypart.js

---
+++ src/components/BodyPart.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+  Stack, Typography
+} from '@mui/material'
+import Icon from '../assets/icons/gym.png'
+const BodyPart = ({data,bodyPart,setBodyPart}) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === data ? "4px solid #ff2625" : '',
+        backgroundColor: "black",
+        borderBottomLeftRadius: "40px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px"
+              
+      }}
+
+      onClick={() => {
+        setBodyPart(data)
+        window.scrollTo({top:1800,behavior:'smooth',left:100})
+      }}
+    >
+      <img src={Icon} style={{ width: "40px", height: "40px" }} alt="" />
+      <Typography sx={{
+        color: '#fff',
+        fontWeight: 'bold',
+        textTransform: 'capitalize',
+        fontSize:'20px'
+      }}>{data}</Typography>
+    </Stack>
+  );
+}
+
+export default BodyPart
