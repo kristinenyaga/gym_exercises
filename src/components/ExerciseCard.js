77df271372fbfa93f4b73d6a2799fd70309fbce1
krisine <kristine.nyaga@student.moringaschool.com>
2023-06-26 22:40:56 +0300

    display an exercise

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Stack, Typography } from '@mui/material'
+
+const ExerciseCard = ({exercise}) => {
+  return (
+    <Link className="exercise-card" to={`/exercise/${exercise.id}`}>
+      <img
+        src={exercise.gifUrl}
+        sx={{
+          borderRadius: "100px",
+        }}
+        loading="lazy"
+        alt="exercises"
+      />
+      <Stack direction="row" sx={{ mt:'20px'}}>
+        <Button
+          sx={{
+            color: "red",
+            bgcolor: "white",
+            fontSize: "14px",
+            textTransform: "capitalize",
+            borderRadius: "20px",
+            ml: "21px",
+            fontWeight: 600,
+            border: "none",
+          }}
+        >
+          {exercise.target}
+        </Button>
+        <Button
+          sx={{
+            color: "#fff",
+            bgcolor: "red",
+            fontSize: "14px",
+            textTransform: "capitalize",
+            borderRadius: "20px",
+            ml: "21px",
+            fontWeight: 600,
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+      </Stack>
+      <Typography sx={{
+        ml: '21px',
+        color: '#fff',
+        fontWeight: 'bold',
+        textTransform: 'capitalize',
+        fontSize: '14px',
+        mt: '11px',
+        pb:'10px'
+      }}>{ exercise.name}</Typography>
+    </Link>
+  );
+}
+
+export default ExerciseCard
